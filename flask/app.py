d3a05bd70e940bee93379e1cc00e81021bf25df0
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-07-10 20:07:59 -0700

    Basic Flask app

---
+++ flask/app.py
@@ -0,0 +1,9 @@
+From flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'GET ME OUT OF THIS THING!!!'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
