9c80569982e7f37431ad559b6b70562a3e5219e7
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-10-26 14:58:54 -0700

    Basic golang app

---
+++ go/app.go
@@ -0,0 +1,24 @@
+package main
+
+import(
+    "fmt"
+    "time"
+)
+
+var current = time.Now()
+var deadline = time.Date(2016, time.November, 8, 0, 0, 0, 0, time.UTC)
+
+func main() {
+    tillElection := deadline.Sub(current)
+    toNanoseconds := tillElection.Nanoseconds()
+    hours := toNanoseconds/3600000000000
+    remainder := toNanoseconds%3600000000000
+    minutes := remainder/60000000000
+    remainder = remainder%60000000000
+    seconds := remainder/1000000000
+    days := hours/24
+    hoursLeft := hours%24
+
+    fmt.Printf("\nHow long until the election?\n\n%v Days %v Hours %v Minutes %v Seconds\n\n", days, hoursLeft, minutes, seconds)
+    
+}
