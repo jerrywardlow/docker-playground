9cd359a847c4954330ebf2b9aa00f0fed891aba1
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-12-18 14:20:22 -0800

    Cross origin support

--- mean/express-server/server.js
+++ mean/express-server/server.js
@@ -10,6 +10,12 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+});
+
 app.use('/', api);
 
 var port = process.env.PORT || '3000';
