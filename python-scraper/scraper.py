da2eb0f87d4677b9d3147cd45fc94b6707cac651
Jerry Wardlow <jerry@jerryoftheyear.com>
2018-01-16 21:44:06 -0800

    Function loops every 10 seconds

--- python-scraper/scraper.py
+++ python-scraper/scraper.py
@@ -3,11 +3,15 @@
 # Library for parsing YAML files
 import yaml
 
+# Threading library
+import threading
+
 # Open configuration file and read YAML contents
 with open('config.yml', 'r') as config_file:
     cfg = yaml.load(config_file)
 
 def cron_loop():
+    threading.Timer(10.0, cron_loop).start()
     print "Placeholder for cron function"
 
 if __name__ == '__main__':
