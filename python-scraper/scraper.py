2699fcfabbece6f9ad0e8c1bc8652349ad677ea2
Jerry Wardlow <jerry@jerryoftheyear.com>
2018-01-16 21:38:32 -0800

    Placeholder/boilerplate

--- python-scraper/scraper.py
+++ python-scraper/scraper.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python
 
+# Library for parsing YAML files
 import yaml
 
 # Open configuration file and read YAML contents
 with open('config.yml', 'r') as config_file:
     cfg = yaml.load(config_file)
+
+def cron_loop():
+    print "Placeholder for cron function"
+
+if __name__ == '__main__':
+    cron_loop()
